61a0f136de3ea815174f3f01e18a932c8249fb70
kloomis1 <kloomis@algorafinance.com>
2017-10-20 21:19:13 -0500

    Organizing VaR script

--- VaR.R
+++ VaR.R
@@ -5,21 +5,71 @@
 #       historical simulation VaR. 
 
 
-# Import Quandl to fetch data
+# Import Quandl library to fetch data
 library(Quandl)
 
-# Why Log Returns? https://quantivity.wordpress.com/2011/02/21/why-log-returns/
-logReturns <- function(priceTS) {
+#' Calculate Log Returns
+#' @param priceTS Price Timeseries
+#' @return Log Returns
+#' @export
+logReturnsTTR <- function(priceTS) {
   return(na.omit(TTR::ROC(priceTS)))
 }
 
-# Fetch Adjusted Close price from 
-# Quandl from start to end date
+#' Calculate Log Returns - w/o external package
+#' @param priceTS Price Timeseries
+#' @return Log Returns  
+#' @export
+logReturns <- function(priceTS) {
+  n <-length(priceTS)
+  i <- 1
+  logged <- c(0)
+  
+  # Calculating logarithmic returns
+  while(i < n) {
+    logged[i] <- log(priceTS[i+1]/priceTS[i],exp(1))
+    i = i + 1
+  }
+  return(logged)
+}
+
+#' Adjusted close prices from WIKI database
+#' @param ticker Stocker ticker
+#' @param start Start date - format: "YYYY-MM-DD"
+#' @param end End date - format: "YYYY-MM-DD"
+#' @return Vector of Adjusted close prices
+#' @export
 adjClose <- function(ticker, start, end) {
   Quandl(paste0("WIKI/",ticker), start_date=start, end_date=end)[,"Adj. Close"]
 }
 
-# calibration and testing price vectors
+#' Parametric Value at Risk (VaR)
+#' @param mean Mean return of timeseries data
+#' @param sigma Standard deviation value of timeseries data
+#' @param alpha Alpha value for confidence level
+#' @param days Number of days to forecast VaR
+#' @return Vector of Adjusted close prices
+#' @export
+parametricVaR <- function(mean, sigma, alpha, days) {
+  # qnorm function used to calculate z-score
+  return((mean - (qnorm(1-alpha,0,1)*sigma))*sqrt(days))
+}
+
+#' Value at Risk Statistics
+#' @param calibrationVaR Calibrated VaR 
+#' @param testMin Minimum return
+#' @return Vector of Calibrated VaR and Minimum Return
+#' @export
+VaRStats <- function(calibrationVaR, testMin) {
+  stats <- c(calibrationVaR, testMin) 
+  names(stats) <- c("Calibrated VaR","Minumum Return")
+  return(stats)
+}
+
+
+# ----------------- TESTING ----------------- 
+
+# Calibration and testing adjusted closing price vectors 
 calibrationPrices <- adjClose("NVDA",start="2002-09-15", "2007-09-14")
 testPrices <- adjClose("NVDA",start="2007-09-15", "2009-09-15")
 
@@ -28,47 +78,19 @@ testPrices <- adjClose("NVDA",start="2007-09-15", "2009-09-15")
 calibrationReturn <- logReturns(calibrationPrices)
 testReturn <- logReturns(testPrices)
 
-# historicalVaR: Historical Simulation Value at Risk
-# z = z-score; a = 0.05 -> z = 1.65
-historicalVaR <- function(mean, sigma, z) {
-  (mean - (z*sigma))
-}
-
+# Calculate mean and standard deviation with return vector
 calibrationMean <- mean(calibrationReturn)
 calibrationSigma <- sd(calibrationReturn)
 
-historicalVaR(calibrationMean, calibrationSigma, 1.65)
+# Calculate VaR with a = 0.05 and time period = 1 day
+calibrationVaR <- parametricVaR(calibrationMean, calibrationSigma, 0.05, 1)
 
-min(testRet)
-
-hVaRStats <- function() {
-  stats <- c(historicalVaR(calibrationMean, calibrationSigma, 1.65), min(testRet)) 
-  names(stats) <- c("Calibrated VaR","Minumum Return")
-  return(stats)
-  }
-
-print(hVaRStats())
-
-logReturn <- function(prices) {
-  n <-length(prices)
-  i <- 1
-  logged <- c(0)
-  
-  # Calculating logarithmic returns
-  while(i < n) {
-    logged[i] <- log(prices[i+1]/prices[i],exp(1))
-    i = i + 1
-  }
-  return(logged)
-}
+# Find minimum value in test return time period
+testMin <- min(testReturn)
 
 
-quantile(logReturn(calibrationPrices),0.05)
-
-qnorm(1-0.01,0,1)*calibrationSigma*sqrt(5)
-
-
-
-
-
-
+# Print Calibrated VaR and Minimum return
+# If abs(calibrationVaR) > abs(testMin), then
+# VaR doesn't account for return during testing phase
+# (lies outside predicted range)
+print(VaRStats(calibrationVaR, testMin))
